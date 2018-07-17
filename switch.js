aec5b1a7aeb9a3c70d780dee498532a67c7e2343
marwanaboudaoud <marwanaboudoud@gmail.com>
2018-07-17 16:16:49 +0200

    switch statement

---
+++ switch.js
@@ -0,0 +1,62 @@
+let Car = "mustang";
+
+switch (Car) {
+    case "BMW":
+          console.log("jack car");
+          break;
+    case "mustang":
+          console.log("my car");
+          break;
+    case "ford":
+          console.log("ellen car");
+          break;
+    default:
+          console.log("we don't know for who this car");
+          break;
+
+}
+
+// my car
+
+car = "ford";
+
+
+switch (Car) {
+    case "BMW":
+          console.log("jack car");
+          break;
+    case "mustang":
+          console.log("my car");
+          break;
+    case "ford":
+          console.log("ellen car");
+          break;
+    default:
+          console.log("we don't know for who this car");
+          break;
+
+}
+
+//ellen car
+
+car = "honda";
+
+switch (Car) {
+    case "BMW":
+          console.log("jack car");
+          break;
+    case "mustang":
+          console.log("my car");
+          break;
+    case "ford":
+          console.log("ellen car");
+          break;
+    default:
+          console.log("we don't know for who this car");
+          break;
+
+}
+
+//we don't know for who this car
+
+
