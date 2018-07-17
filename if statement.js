15a288b00aa2e77e362e63d85f282076d129222c
marwanaboudaoud <marwanaboudoud@gmail.com>
2018-07-17 15:59:07 +0200

    if statement

---
+++ if statement.js
@@ -0,0 +1,97 @@
+let a = 100;
+let b = 50;
+
+if (a > b) {
+  console.log("that is true");
+} if (a === b) {
+  console.log("that is false")
+} if (a < b ){
+  console.log("that is false")
+}
+//that is true
+
+b = 150;
+
+if (a > b) {
+    console.log("that is true");
+  }else if (a === b) {
+    console.log("that is false")
+  }else if (a < b ){
+    console.log("that is false")
+  }
+
+  //that is false
+
+  // short way to make if statement
+
+if (a > b){
+    console.log("that is true")
+}else{
+    console.log("that is false")
+}
+
+//that is true
+
+
+// other example
+
+let distance = 1;
+
+if (distance < 2){
+    console.log ("i will walk.")
+}else if (distance < 10){
+    console.log ("i will take bike.")
+}else if (distance < 50){
+    console.log ("i will go by car.")
+}else {
+    console.log ("i will take the train")
+}
+
+//i will walk.
+
+distance = 5;
+
+if (distance < 2){
+    console.log ("i will walk.")
+}else if (distance < 10){
+    console.log ("i will take bike.")
+}else if (distance < 50){
+    console.log ("i will go by car.")
+}else {
+    console.log ("i will take the train")
+}
+
+//i will take bike.
+
+distance = 15;
+
+if (distance < 2){
+    console.log ("i will walk.")
+}else if (distance < 10){
+    console.log ("i will take bike.")
+}else if (distance < 50){
+    console.log ("i will go by car.")
+}else {
+    console.log ("i will take the train")
+}
+
+//i will go by car.
+
+distance = 55;
+
+if (distance < 2){
+    console.log ("i will walk.")
+}else if (distance < 10){
+    console.log ("i will take bike.")
+}else if (distance < 50){
+    console.log ("i will go by car.")
+}else {
+    console.log ("i will take the train")
+}
+
+//i will take the train
+
+
+
+
+
