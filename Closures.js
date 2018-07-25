a2cc7279cb7ecef0e93b2d71207d42e6c5a31238
marwanaboudaoud <marwanaboudoud@gmail.com>
2018-07-25 12:58:59 +0200

    cloosures

--- Closures.js
+++ Closures.js
@@ -31,5 +31,30 @@ function showName (firstName, lastName) {
     showName ("Michael", "Jackson"); 
     //"Your name is Michael Jackson"
 
+//************************************************************************** */
+    function init() {
+        var name = 'Mozilla'; // name is a local variable created by init
+        function displayName() { // displayName() is the inner function, a closure
+          console.log(name); // use variable declared in the parent function    
+        }
+        displayName();    
+      }
+      init();
+//Mozilla
 
-    
+//Here's a slightly more interesting example
+
+function makeAdder(x) {
+    return function(y) {
+      return x + y;
+    };
+  }
+  
+  var add5 = makeAdder(5);
+  var add10 = makeAdder(10);
+  
+  console.log(add5(2));  // 7
+  console.log(add10(2)); // 12
+
+  
+
